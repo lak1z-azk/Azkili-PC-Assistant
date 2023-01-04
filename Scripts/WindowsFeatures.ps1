9ac6952ca3416cd3dc192d734981a2f2b0f9cdf1
Lak1z <100957521+Lak1zz@users.noreply.github.com>
2023-01-04 19:35:12 +0200

    Added Error Handle

--- Scripts/WindowsFeatures.ps1
+++ Scripts/WindowsFeatures.ps1
@@ -1,37 +1,39 @@
-﻿Start-Transcript -Path C:\WindowsFeaturesLogs.txt
-Write-Host "`n-------------------------------------------------------------------"
+# Start a transcript of the session and save it to a file
+Start-Transcript -Path C:\WindowsFeaturesLogs.txt
 Write-Host -ForegroundColor Yellow "Logs will be saved in C:\WindowsFeaturesLogs.txt" 
-Write-Host "-------------------------------------------------------------------"
-sleep 2
 
-#################WINDOWS FEATURES ENABLE
-Write-Host "`n-------------------------------------------------------------------"
+# Enable Windows features
 Write-Host -ForegroundColor Yellow "Enabling Windows Features"
-Write-Host "-------------------------------------------------------------------"
+try {
+  # Enable the NetFx3 and SMB1Protocol features
+  Enable-WindowsOptionalFeature -Online -FeatureName "NetFx3" -All
+  Enable-WindowsOptionalFeature -Online -FeatureName "SMB1Protocol" -All
+}
+# Catch any errors that occur while enabling the features
+catch {
+  # Write an error message to the transcript
+  Write-Error "An error occurred while enabling features: $($_.Exception.Message)"
+}
 
-sleep 1 
-
-Enable-WindowsOptionalFeature -Online -FeatureName "NetFx3" -All
-Enable-WindowsOptionalFeature -Online -FeatureName "SMB1Protocol" -All
-sleep 2
-
-
-Write-Host "`n-------------------------------------------------------------------"
+# Install Windows extensions
 Write-Host -ForegroundColor Yellow "Installing Windows Extensions... "
-Write-Host "-------------------------------------------------------------------"
-Start-Sleep -Seconds 3
-choco install vcredist-all chocolatey-windowsupdate.extension dotnet -y
-Start-Sleep -Seconds 3
-Write-Host "`n-------------------------------------------------------------------"
-Write-Host -ForegroundColor Yellow "Installation finished.. "
-Write-Host "-------------------------------------------------------------------"
-Start-Sleep -Seconds 3
+try {
+  # Install the vcredist-all, chocolatey-windowsupdate.extension, and dotnet packages
+  choco install vcredist-all chocolatey-windowsupdate.extension dotnet -y
 
+  Write-Host -ForegroundColor Yellow "Installation finished.. "
+}
+# Catch any errors that occur while installing the packages
+catch {
+  # Write an error message to the transcript
+  Write-Error "An error occurred while installing extensions: $($_.Exception.Message)"
+}
 
+# Display a message indicating that the installation has finished
 Write-Host -ForegroundColor Yellow "Installation has finished... Closing script"
 
+# Stop the transcript
 Stop-Transcript
 
+# Wait for 5 seconds before closing the script
 sleep 5
-
-
