e6969b44af7f14ff8538ec2176931ab4cb390310
Lak1z <100957521+lak1z-azk@users.noreply.github.com>
2024-08-29 18:36:14 +0300

    Delete Scripts directory

--- Scripts/Optional.bat
+++
@@ -1,5 +0,0 @@
-@Echo off
-PowerShell -WindowStyle Hidden -NoProfile -ExecutionPolicy Bypass -Command "& {Start-Process PowerShell -ArgumentList '-NoProfile -ExecutionPolicy Bypass -File ""%~dp0Optional.ps1""' -Verb RunAs}"
-
-
-Exit
--- Scripts/WindowsFeatures.bat
+++
@@ -1,5 +0,0 @@
-@Echo off
-PowerShell -WindowStyle Hidden  -NoProfile -ExecutionPolicy Bypass -Command "& {Start-Process PowerShell -ArgumentList '-NoProfile -ExecutionPolicy Bypass -File ""%~dp0WindowsFeatures.ps1""' -windowstyle minimized -Verb RunAs}"
-
-
-Exit
